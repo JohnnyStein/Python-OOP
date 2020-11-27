f4188de160b81f8ea78b54ed436f19108b4747eb
JohnnyStein <pedroheringer945@gmail.com>
2020-11-27 12:13:57 +0000

    Fish and Dog

--- pets.py
+++ pets.py
@@ -6,6 +6,13 @@ class Dog(Pet):
         self.name = name
         self.age = age
         self.type = "Dog"
+class Fish(Pet):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.type = "Fish"
 
 a1 = Dog("Bob", 7)
 a1.show()
+a2 = Fish("Tim", 3)
+a2.show()
